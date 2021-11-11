9788958c04da95b3ec3478bb3d81317b330a855b
baecheese <jiyoung.m.bae@gmail.com>
2021-11-11 16:42:08 +0900

    SuperPayDashboardInteractor에 비즈니스 로직 추가 - 잔액 데이터를 표시할 수 있도록 SuperPayDashboardInteractorDependency 추가

--- MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardInteractor.swift
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardInteractor.swift
@@ -8,37 +8,46 @@
 import ModernRIBs
 
 protocol SuperPayDashboardRouting: ViewableRouting {
-    // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
+  // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
 }
 
 protocol SuperPayDashboardPresentable: Presentable {
-    var listener: SuperPayDashboardPresentableListener? { get set }
-    // TODO: Declare methods the interactor can invoke the presenter to present data.
+  var listener: SuperPayDashboardPresentableListener? { get set }
+  // TODO: Declare methods the interactor can invoke the presenter to present data.
 }
 
 protocol SuperPayDashboardListener: AnyObject {
-    // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+  // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+}
+
+// 추가되는 사항에 유연하게 대응하기 위해 init인자로 다 넣지 않고, 이렇게 protocol 내에서 추가한다.
+protocol SuperPayDashboardInteractorDependency {
+  var balance: ReadOnlyCurrentValuePublisher<Double> { get }
 }
 
 final class SuperPayDashboardInteractor: PresentableInteractor<SuperPayDashboardPresentable>, SuperPayDashboardInteractable, SuperPayDashboardPresentableListener {
-
-    weak var router: SuperPayDashboardRouting?
-    weak var listener: SuperPayDashboardListener?
-
-    // TODO: Add additional dependencies to constructor. Do not perform any logic
-    // in constructor.
-    override init(presenter: SuperPayDashboardPresentable) {
-        super.init(presenter: presenter)
-        presenter.listener = self
-    }
-
-    override func didBecomeActive() {
-        super.didBecomeActive()
-        // TODO: Implement business logic here.
-    }
-
-    override func willResignActive() {
-        super.willResignActive()
-        // TODO: Pause any business logic.
-    }
+  
+  weak var router: SuperPayDashboardRouting?
+  weak var listener: SuperPayDashboardListener?
+  
+  private let dependency: SuperPayDashboardInteractorDependency
+  
+  init(
+    presenter: SuperPayDashboardPresentable,
+    dependency: SuperPayDashboardInteractorDependency
+  ) {
+    self.dependency = dependency
+    super.init(presenter: presenter)
+    presenter.listener = self
+  }
+  
+  override func didBecomeActive() {
+    super.didBecomeActive()
+    // TODO: Implement business logic here.
+  }
+  
+  override func willResignActive() {
+    super.willResignActive()
+    // TODO: Pause any business logic.
+  }
 }
