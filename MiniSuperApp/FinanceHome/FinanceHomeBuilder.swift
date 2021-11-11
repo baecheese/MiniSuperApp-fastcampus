04098bcaea4a80869475d4857d2baa86dd0a51a8
baecheese <jiyoung.m.bae@gmail.com>
2021-11-11 15:04:26 +0900

    FinanceHomeBuilder에서 SuperPayBashBoard의 Dependency를 받을 수 있도록 한다.

--- MiniSuperApp/FinanceHome/FinanceHomeBuilder.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeBuilder.swift
@@ -1,11 +1,11 @@
-import ModernRIBs
+ import ModernRIBs
 
 protocol FinanceHomeDependency: Dependency {
   // TODO: Declare the set of dependencies required by this RIB, but cannot be
   // created by this RIB.
 }
 
-final class FinanceHomeComponent: Component<FinanceHomeDependency> {
+final class FinanceHomeComponent: Component<FinanceHomeDependency>, SuperPayDashboardDependency {
   
   // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
 }
@@ -23,10 +23,16 @@ final class FinanceHomeBuilder: Builder<FinanceHomeDependency>, FinanceHomeBuild
   }
   
   func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting {
-    let _ = FinanceHomeComponent(dependency: dependency)
+    // riblet의 component는 riblet에 필요한 객체를 담는 바구니
+    // 이 component는 자식 riblet이 필요한 것도 담는다.
+    // 때문에 자식인 SuperPayDashboardDependency를 부모 FinanceHomeComponent가 상속 받음
+    let component = FinanceHomeComponent(dependency: dependency)
     let viewController = FinanceHomeViewController()
     let interactor = FinanceHomeInteractor(presenter: viewController)
     interactor.listener = listener
+    
+    let superPayDashboardBuilder = SuperPayDashboardBuilder(dependency: component)
+    
     return FinanceHomeRouter(interactor: interactor, viewController: viewController)
   }
 }
