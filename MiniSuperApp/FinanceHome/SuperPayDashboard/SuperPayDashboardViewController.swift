854d9467b542ea64156ade9247a1991982a9dda2
baecheese <jiyoung.m.bae@gmail.com>
2021-11-11 15:59:45 +0900

    슈퍼페이 화면 상단 titleLabel, Button 생성

--- MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
@@ -9,12 +9,69 @@ import ModernRIBs
 import UIKit
 
 protocol SuperPayDashboardPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+  // TODO: Declare properties and methods that the view controller can invoke to perform
+  // business logic, such as signIn(). This protocol is implemented by the corresponding
+  // interactor class.
 }
 
 final class SuperPayDashboardViewController: UIViewController, SuperPayDashboardPresentable, SuperPayDashboardViewControllable {
-
-    weak var listener: SuperPayDashboardPresentableListener?
+  
+  weak var listener: SuperPayDashboardPresentableListener?
+  
+  private let headerStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.alignment = .fill
+    stackView.distribution = .equalSpacing
+    stackView.axis = .horizontal
+    return stackView
+  }()
+  
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.font = .systemFont(ofSize: 22.0, weight: .semibold)
+    label.text = "슈퍼페이 잔고"
+    return label
+  }()
+  
+  private lazy var topupButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("충전하기", for: .normal)
+    button.setTitleColor(.systemBlue, for: .normal)
+    // target에 self가 들어가야해서 lazy로
+    button.addTarget(self, action: #selector(topupButtonDidTap), for: .touchUpInside)
+    return button
+  }()
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    
+    setupViews()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    
+    setupViews()
+  }
+  
+  private func setupViews() {
+    view.addSubview(headerStackView)
+    
+    headerStackView.addArrangedSubview(titleLabel)
+    headerStackView.addArrangedSubview(topupButton)
+    
+    NSLayoutConstraint.activate([
+      headerStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10.0),
+      headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20.0),
+      headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20.0)
+    ])
+  }
+  
+  @objc func topupButtonDidTap() {
+    
+  }
+  
 }
