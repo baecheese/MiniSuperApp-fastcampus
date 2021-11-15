89cc423e2c61dce4a0063e7fe9ee78f58bfb0221
baecheese <jiyoung.m.bae@gmail.com>
2021-11-15 15:12:36 +0900

    ViewModel 생성 - ViewController에서는 최대한 넘겨받은 것을 그리는 역할만 줄 것. 변환하는 로직은 ViewModel로 넘기려 한다.

---
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/PaymentMethodViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  PaymentMethodViewModel.swift
+//  MiniSuperApp
+//
+//  Created by 배지영 on 2021/11/15.
+//
+
+import UIKit
+
+struct PaymentMethodViewModel {
+  
+  let name: String
+  let digits: String
+  let color: UIColor // backend에서 보통 hex string으로 내려준다
+  
+  init(_ paymentMethod: PaymentMethod) {
+    name = paymentMethod.name
+    digits = paymentMethod.digits
+    color = UIColor(hex: paymentMethod.color) ?? .systemGray2
+  }
+  
+}
