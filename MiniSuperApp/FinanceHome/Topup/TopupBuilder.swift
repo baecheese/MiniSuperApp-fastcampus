ac6726a3888c2c0346fbe9f247a2532d488d6f71
baecheese <jiyoung.m.bae@gmail.com>
2021-11-18 13:29:59 +0900

    topupBuilder에서 component에 dependency 추가

--- MiniSuperApp/FinanceHome/Topup/TopupBuilder.swift
+++ MiniSuperApp/FinanceHome/Topup/TopupBuilder.swift
@@ -11,33 +11,34 @@ import ModernRIBs
 /// topup riblet을 띄운 부모가 지정
 protocol TopupDependency: Dependency {
   var topupBaseViewController: ViewControllable { get }
+  var cardOnFileRepository: CardOnFileRepository { get }
 }
 
-final class TopupComponent: Component<TopupDependency> {
-
-    fileprivate var topupBaseViewController: ViewControllable {
-        return dependency.topupBaseViewController
-    }
-
-    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+final class TopupComponent: Component<TopupDependency>, TopupInteractorDependency {
+  var cardOnFileRepository: CardOnFileRepository {
+    dependency.cardOnFileRepository
+  }
+  fileprivate var topupBaseViewController: ViewControllable {
+    dependency.topupBaseViewController
+  }
 }
 
 // MARK: - Builder
 
 protocol TopupBuildable: Buildable {
-    func build(withListener listener: TopupListener) -> TopupRouting
+  func build(withListener listener: TopupListener) -> TopupRouting
 }
 
 final class TopupBuilder: Builder<TopupDependency>, TopupBuildable {
-
-    override init(dependency: TopupDependency) {
-        super.init(dependency: dependency)
-    }
-
-    func build(withListener listener: TopupListener) -> TopupRouting {
-        let component = TopupComponent(dependency: dependency)
-        let interactor = TopupInteractor()
-        interactor.listener = listener
-        return TopupRouter(interactor: interactor, viewController: component.topupBaseViewController)
-    }
+  
+  override init(dependency: TopupDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  func build(withListener listener: TopupListener) -> TopupRouting {
+    let component = TopupComponent(dependency: dependency)
+    let interactor = TopupInteractor(dependency: component)
+    interactor.listener = listener
+    return TopupRouter(interactor: interactor, viewController: component.topupBaseViewController)
+  }
 }
