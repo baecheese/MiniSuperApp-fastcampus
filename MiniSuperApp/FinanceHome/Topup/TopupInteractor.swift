aa90b8278fc728bb07ae74ae28c2605f1f03423a
baecheese <jiyoung.m.bae@gmail.com>
2021-11-18 13:25:05 +0900

    카드에 갯수에 따라 다른 화면을 띄우는 로직 추가 - interactor에서 interactor dependency 추가

--- MiniSuperApp/FinanceHome/Topup/TopupInteractor.swift
+++ MiniSuperApp/FinanceHome/Topup/TopupInteractor.swift
@@ -16,18 +16,26 @@ protocol TopupListener: AnyObject {
   // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
 }
 
+protocol TopupInteractorDependency {
+  var cardOnFileRepository: CardOnFileRepository { get }
+}
+
 final class TopupInteractor: Interactor, TopupInteractable {
   
   weak var router: TopupRouting?
   weak var listener: TopupListener?
   
-  // TODO: Add additional dependencies to constructor. Do not perform any logic
-  // in constructor.
-  override init() {}
+  private let dependency: TopupInteractorDependency
+  
+  init(
+    dependency: TopupInteractorDependency
+  ) {
+    self.dependency = dependency
+  }
   
   override func didBecomeActive() {
     super.didBecomeActive()
-    print("topup didBecomeActive")
+    // 카드에 갯수에 따라 다른 화면을 띄우는 로직
   }
   
   override func willResignActive() {
