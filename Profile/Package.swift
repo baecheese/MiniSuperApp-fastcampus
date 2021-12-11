858481d05aea1b8e312e2f9436b688d0e9b3f337
baecheese <jiyoung.m.bae@gmail.com>
2021-12-11 22:19:26 +0900

    Profile 패키지에 ProfileHome 추가

--- Profile/Package.swift
+++ Profile/Package.swift
@@ -4,25 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "Profile",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Profile",
-            targets: ["Profile"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Profile",
-            dependencies: []),
-        .testTarget(
-            name: "ProfileTests",
-            dependencies: ["Profile"]),
-    ]
+  name: "Profile",
+  platforms: [.iOS(.v14)],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "ProfileHome",
+      targets: ["ProfileHome"]),
+  ],
+  dependencies: [
+    .package(name: "ModernRIBs", url: "https://github.com/DevYeom/ModernRIBs", .exact("1.0.1")),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "ProfileHome",
+      dependencies: [
+        "ModernRIBs"
+      ]),
+  ]
 )
--- Profile/Sources/ProfileHome/ProfileHomeBuilder.swift
+++ Profile/Sources/ProfileHome/ProfileHomeBuilder.swift
@@ -1,28 +1,28 @@
 import ModernRIBs
 
-protocol ProfileHomeDependency: Dependency {
+public protocol ProfileHomeDependency: Dependency {
   // TODO: Declare the set of dependencies required by this RIB, but cannot be
   // created by this RIB.
 }
 
-final class ProfileHomeComponent: Component<ProfileHomeDependency> {
+public final class ProfileHomeComponent: Component<ProfileHomeDependency> {
   
   // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
 }
 
 // MARK: - Builder
 
-protocol ProfileHomeBuildable: Buildable {
-  func build(withListener listener: ProfileHomeListener) -> ProfileHomeRouting
+public protocol ProfileHomeBuildable: Buildable {
+  func build(withListener listener: ProfileHomeListener) -> ViewableRouting
 }
 
-final class ProfileHomeBuilder: Builder<ProfileHomeDependency>, ProfileHomeBuildable {
+public final class ProfileHomeBuilder: Builder<ProfileHomeDependency>, ProfileHomeBuildable {
   
-  override init(dependency: ProfileHomeDependency) {
+  public override init(dependency: ProfileHomeDependency) {
     super.init(dependency: dependency)
   }
   
-  func build(withListener listener: ProfileHomeListener) -> ProfileHomeRouting {
+  public func build(withListener listener: ProfileHomeListener) -> ViewableRouting {
     let _ = ProfileHomeComponent(dependency: dependency)
     let viewController = ProfileHomeViewController()
     let interactor = ProfileHomeInteractor(presenter: viewController)
