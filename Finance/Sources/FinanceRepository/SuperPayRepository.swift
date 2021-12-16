6a31ddd22b302055bda108d35bdb6ca4af333e2d
baecheese <jiyoung.m.bae@gmail.com>
2021-12-16 21:43:33 +0900

    서버에서 받아와야했던 코드들 newtork 객체를 통해 업데이트 되는 구조로 수정

--- Finance/Sources/FinanceRepository/SuperPayRepository.swift
+++ Finance/Sources/FinanceRepository/SuperPayRepository.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Combine
 import CombineUtil
+import Network
 
 public protocol SuperPayRepository {
   var balance: ReadOnlyCurrentValuePublisher<Double> { get }
@@ -21,19 +22,30 @@ public class SuperPayRepositoryImp: SuperPayRepository {
   // 원래였으면 서버에서 받아와야 하지만, 이 예제 프로젝트에서는 초기값을 준다.
   private let balanceSubject = CurrentValuePublisher<Double>(0.0)
   
-  public init() { }
-  
   public func topup(amount: Double, paymentId: String) -> AnyPublisher<Void, Error> {
-    // 서버가 없어서 보이기에 call back 효과를 주기위한 코드
-    return Future<Void, Error> { [weak self] promise in
-      self?.bgQueue.async {
-        Thread.sleep(forTimeInterval: 2.0)
-        promise(.success(()))
-        let newBalanceSubject = (self?.balanceSubject.value).map { $0 + amount }
-        newBalanceSubject.map { self?.balanceSubject.send($0) }
-      }
-    }.eraseToAnyPublisher()
+    let request = TopupRequest(baseURL: baseURL, amount: amount, paymentMethod: paymentId)
+    return network.send(request)
+      .handleEvents( // 서버가 없기 때문에 수동으로 넣어줌
+        receiveSubscription: nil,
+        receiveOutput: { [weak self] _ in
+          let newBalance = (self?.balanceSubject.value).map { $0 + amount }
+          newBalance.map { self?.balanceSubject.send($0) }          
+        },
+        receiveCompletion: nil,
+        receiveCancel: nil,
+        receiveRequest: nil
+      )
+      .map { _ in }
+      .eraseToAnyPublisher()
   }
   
   private let bgQueue: DispatchQueue = DispatchQueue(label: "topup.repository.queue")
+  
+  private let network: Network
+  private let baseURL: URL
+  
+  public init(network: Network, baseURL: URL) {
+    self.network = network
+    self.baseURL = baseURL
+  }
 }
