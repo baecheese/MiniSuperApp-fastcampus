5588b77ede09207fa65962a98ed196e67393f8b6
jiyoung <jiyoung.m.bae@gmail.com>
2022-01-07 17:21:24 +0900

    AppRootComponent에 network url session을 직접 만든 mock configureation 넣을 수 있도록 수정

--- MiniSuperApp/AppRoot/AppRootComponent.swift
+++ MiniSuperApp/AppRoot/AppRootComponent.swift
@@ -51,11 +51,12 @@ final class AppRootComponent: Component<AppRootDependency>, AppHomeDependency, T
     urlConfiguration.protocolClasses = [SuperAppURLProtocol.self]
     setupURLProtocol()
     
-    let network = NetworkImp(session: URLSession.shared)
+    let network = NetworkImp(session: URLSession(configuration: urlConfiguration))
     self.cardOnFileRepository = CardOnFileRepositoryImp(network: network, baseURL: BaseURL().financeBaseURL)
     self.cardOnFileRepository.fetch()
     self.superPayRepository = SuperPayRepositoryImp(network: network, baseURL: BaseURL().financeBaseURL)
     self.rootViewController = rootViewController
     super.init(dependency: dependency)
+    
   }
 }
