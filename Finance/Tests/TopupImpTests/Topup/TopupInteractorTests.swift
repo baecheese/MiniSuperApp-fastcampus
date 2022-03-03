905131f3ba76eaced9e408ad0250cd866f211bf3
jiyoung <jiyoung.m.bae@gmail.com>
2022-03-03 14:42:22 +0900

    topup 테스트 코드 파일 생성

---
+++ Finance/Tests/TopupImpTests/Topup/TopupInteractorTests.swift
@@ -0,0 +1,29 @@
+//
+//  TopupInteractorTests.swift
+//  MiniSuperApp
+//
+//  Created by 배지영 on 2022/03/03.
+//
+
+@testable import TopupImp
+import XCTest
+
+final class TopupInteractorTests: XCTestCase {
+
+    private var interactor: TopupInteractor!
+
+    // TODO: declare other objects and mocks you need as private vars
+
+    override func setUp() {
+        super.setUp()
+
+        // TODO: instantiate objects and mocks
+    }
+
+    // MARK: - Tests
+
+    func test_exampleObservable_callsRouterOrListener_exampleProtocol() {
+        // This is an example of an interactor test case.
+        // Test your interactor binds observables and sends messages to router or listener.
+    }
+}
